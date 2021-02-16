362ee86be2c2a2bdd807f3ce2e732bbbd2dfc2b4
BeeCabs <beecabido@gmail.com>
2021-02-16 08:36:40 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/act_3_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.act_3_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
